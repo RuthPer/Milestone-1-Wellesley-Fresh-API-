57e32453fff0d20d0f47edf246c5391a92cb877f
RuthPer <rp104@wellesley.edu>
2025-04-01 14:20:14 -0400

    Added date

--- streamlitCode.py
+++ streamlitCode.py
@@ -1,2 +1,9 @@
 import streamlit as st
+import datetime
+import Milestone1
 
+st.title("Food App-Milestone 1")
+st.write("MileStone")
+
+d = st.date_input("Meal Option date?", datetime.date(2025, 4, 1))
+st.write("You Selected", d)
