a38e7804a85c9cc81faa9af368e9e966b53099a2
Dianna <dg109@wellesley.edu>
2025-04-01 13:40:05 -0400

    wellesley api code

--- Milestone1.py
+++ Milestone1.py
@@ -1,1 +1,39 @@
 #Milestone1
+
+import requests
+import json
+import csv
+
+locations = ['Bao', 'Bates', 'StoneD', 'Tower']
+meals = ['Breakfast', 'Lunch', 'Dinner']
+locationIDs = {'Bao': 96, 
+             'Bates': 95, 
+             'StoneD': 131, 
+             'Tower': 97
+             }
+mealIDs = {'Bao': {'Breakfast': 148, 'Lunch': 149, 'Dinner': 312}, 
+         'Bates': {'Breakfast': 145, 'Lunch': 146, 'Dinner': 311},
+         'StoneD': {'Breakfast': 261, 'Lunch': 262, 'Dinner': 263}, 
+         'Tower': {'Breakfast': 153, 'Lunch': 154, 'Dinner': 310}
+         }
+
+with open ('wellesley-dining.csv', 'w',newline='') as file:
+    fieldnames = ['location','meal','locationID','mealID']
+    writer = csv.DictWriter(file, fieldnames=fieldnames)
+    writer.writeheader()
+
+    for l in locations:
+        for m in meals:
+            dct = {'location':l, 'meal':m, 'locationID':locationIDs[l], 'mealID':mealIDs[l][m]}
+            print(dct)
+            writer.writerow(dct)
+            
+def get_menu(date, locID, mealID):
+    baseURL = "https://dish.avifoodsystems.com/api/menu-items/week"
+    params = {'date': date,
+              'locationID': locID,
+              'mealID': mealID}
+
+    response = requests.get(baseURL, params=params)
+    data = response.json()
+    return data
